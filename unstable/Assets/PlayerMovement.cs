dae0eee76d19e7a5f61120ebefbea78e2d6d6f5c
Kristofer Carlsson <kristofer.robert.carlsson@gmail.com>
2021-10-02 15:59:58 +0200

    Empty project

---
+++ unstable/Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private Rigidbody2D rb;
+    private bool facingRight = true;
+    private float movingDirection;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+   }
+
+//    void Start()
+//    {
+//    }
+
+
+    void Update()
+    {
+        movingDirection = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(movingDirection * moveSpeed, rb.velocity.y);
+    }
+}
